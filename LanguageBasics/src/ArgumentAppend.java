3d8f3463d2f9e9d5dc1a2319b98500957074234f
AjithKumarT <ajithsubramanian59@gmail.com>
2019-12-14 21:13:27 +0530

    deleting all files

--- LanguageBasics/src/ArgumentAppend.java
+++
@@ -1,8 +0,0 @@
-import java.util.Scanner;
-
-public class ArgumentAppend {
-public static void main(String args[]) {
-	Scanner s= new Scanner(System.in);
-	System.out.println("Welcome " +args[0] +" Entered input is "+s.nextLine());
-}
-}
